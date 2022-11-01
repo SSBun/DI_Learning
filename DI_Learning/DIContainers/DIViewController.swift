62ecc6323ce4bbbd26476d6dde152d2a0202fb34
caishilin <caishilin@zhihu.com>
2022-11-01 18:55:22 +0800

    Resolver

--- DI_Learning/DIContainers/DIViewController.swift
+++ DI_Learning/DIContainers/DIViewController.swift
@@ -9,10 +9,17 @@ import UIKit
 
 class DIViewController: UIViewController {
     private let titleLabel = UILabel()
+    private let lock = RecursiveLock()
+    private var arr = [0]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
+        (0...9999).forEach { _ in
+            DispatchQueue.global().async {
+                self.anotherTest()
+            }
+        }
     }
     
     private func setupUI() {
@@ -25,4 +32,39 @@ class DIViewController: UIViewController {
         }
         titleLabel.text = NSStringFromClass(Self.self)
     }
+
+    private func anotherTest() {
+//        lock.lock()
+//        defer { lock.unlock() }
+        testLock()
+    }
+    
+    private func testLock() {
+        lock.lock()
+        defer { lock.unlock() }
+        if Bool.random() {
+           arr.append(1)
+        } else {
+            arr.append(2)
+        }
+//        print(self.count)
+    }
+}
+
+final class RecursiveLock {
+    init() {
+        pthread_mutexattr_init(&recursiveMutexAttr)
+        pthread_mutexattr_settype(&recursiveMutexAttr, PTHREAD_MUTEX_RECURSIVE)
+        pthread_mutex_init(&recursiveMutex, &recursiveMutexAttr)
+    }
+    @inline(__always)
+    final func lock() {
+        pthread_mutex_lock(&recursiveMutex)
+    }
+    @inline(__always)
+    final func unlock() {
+        pthread_mutex_unlock(&recursiveMutex)
+    }
+    private var recursiveMutex = pthread_mutex_t()
+    private var recursiveMutexAttr = pthread_mutexattr_t()
 }
--- DI_Learning/DIContainers/Resolver/ResolverDI.swift
+++ DI_Learning/DIContainers/Resolver/ResolverDI.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import Resolver
+
+class MyResolver: Resolver {
+    deinit {
+        print("resolver release")
+    }
+}
 
 class ResolverDI: DIViewController {
+    
+    private let resolver = MyResolver()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        registerDependencies()
+        resolveDependencies()
     }
     
-    private func setupUI() {
-        
+    private func registerDependencies() {
+        resolver.register {
+            Room()
+        }
+        resolver.register {
+            Person(room: $0.resolve())
+        }
     }
+    
+    private func resolveDependencies() {
+        let person: Person = resolver.resolve()
+        print(person)
+    }
+}
+
+
+private struct Room {
+    
+}
+
+private struct Person {
+    let room: Room
+}
+
+private class Dog {
+    
 }
